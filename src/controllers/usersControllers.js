8bdf7a756df5f08d53d640154b24de4400b75199
Brian Ojeda <brianojedadiaz1204@gmail.com>
2024-04-24 03:04:13 -0500

    new table created

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -2,7 +2,7 @@ import { pool } from "../db.js";
 
 export const userGet = async (req, res) => {
   try {
-    const [usersRows] = await pool.query("SELECT * FROM users");
+    const [usersRows] = await pool.query("SELECT * FROM users_tb");
     res.render("users", { users: usersRows });
   } catch (error) {
     console.log(error);
@@ -14,10 +14,10 @@ export const userCreate = async (req, res) => {
   try {
     const { name, email, phone } = req.body;
     await pool.query(
-      "INSERT INTO users (user_name, user_email, user_phone) VALUES (?,?,?)",
+      "INSERT INTO users_tb (user_name, user_email, user_phone) VALUES (?,?,?)",
       [name, email, phone]
     );
-    res.redirect("/");
+    res.redirect("/home");
   } catch (error) {
     console.log(error);
     res.status(500).send("Internal Server Error");
@@ -28,7 +28,7 @@ export const userGetUpdate = async (req, res) => {
   try {
     const { id } = req.params;
     const [usersRows] = await pool.query(
-      "SELECT * FROM users WHERE user_id = ?",
+      "SELECT * FROM users_tb WHERE user_id = ?",
       [id]
     );
     res.render("users_edit", { users: usersRows[0] });
@@ -43,10 +43,10 @@ export const userUpdate = async (req, res) => {
     const { id } = req.params;
     const { name, email, phone } = req.body;
     await pool.query(
-      "UPDATE users SET user_name = ?, user_email = ?, user_phone = ? WHERE user_id = ?",
+      "UPDATE users_tb SET user_name = ?, user_email = ?, user_phone = ? WHERE user_id = ?",
       [name, email, phone, id]
     );
-    res.redirect("/");
+    res.redirect("/home");
   } catch (error) {
     console.log(error);
     res.status(500).send("Internal Server Error");
@@ -56,8 +56,8 @@ export const userUpdate = async (req, res) => {
 export const userDelete = async (req, res) => {
   try {
     const { id } = req.params;
-    await pool.query("DELETE FROM users WHERE user_id= ?", [id]);
-    res.redirect("/");
+    await pool.query("DELETE FROM users_tb WHERE user_id= ?", [id]);
+    res.redirect("/home");
   } catch (error) {
     res.status(500).send("Internal Server Error");
   }
