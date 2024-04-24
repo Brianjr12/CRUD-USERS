929e1b0932a0f9e2d989225eef55a9a7c64668ce
Brian Ojeda <brianojedadiaz1204@gmail.com>
2024-04-24 03:28:25 -0500

    new table

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -2,7 +2,7 @@ import { pool } from "../db.js";
 
 export const userGet = async (req, res) => {
   try {
-    const [usersRows] = await pool.query("SELECT * FROM users_tb");
+    const [usersRows] = await pool.query("SELECT * FROM users_TB");
     res.render("users", { users: usersRows });
   } catch (error) {
     console.log(error);
@@ -14,7 +14,7 @@ export const userCreate = async (req, res) => {
   try {
     const { name, email, phone } = req.body;
     await pool.query(
-      "INSERT INTO users_tb (user_name, user_email, user_phone) VALUES (?,?,?)",
+      "INSERT INTO users_TB (user_name, user_email, user_phone) VALUES (?,?,?)",
       [name, email, phone]
     );
     res.redirect("/home");
@@ -28,7 +28,7 @@ export const userGetUpdate = async (req, res) => {
   try {
     const { id } = req.params;
     const [usersRows] = await pool.query(
-      "SELECT * FROM users_tb WHERE user_id = ?",
+      "SELECT * FROM users_TB WHERE user_id = ?",
       [id]
     );
     res.render("users_edit", { users: usersRows[0] });
@@ -43,7 +43,7 @@ export const userUpdate = async (req, res) => {
     const { id } = req.params;
     const { name, email, phone } = req.body;
     await pool.query(
-      "UPDATE users_tb SET user_name = ?, user_email = ?, user_phone = ? WHERE user_id = ?",
+      "UPDATE users_TB SET user_name = ?, user_email = ?, user_phone = ? WHERE user_id = ?",
       [name, email, phone, id]
     );
     res.redirect("/home");
@@ -56,7 +56,7 @@ export const userUpdate = async (req, res) => {
 export const userDelete = async (req, res) => {
   try {
     const { id } = req.params;
-    await pool.query("DELETE FROM users_tb WHERE user_id= ?", [id]);
+    await pool.query("DELETE FROM users_TB WHERE user_id= ?", [id]);
     res.redirect("/home");
   } catch (error) {
     res.status(500).send("Internal Server Error");
